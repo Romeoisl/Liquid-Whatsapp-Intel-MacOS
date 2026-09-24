75586bbfeff7f80e9cc210a35d4839e6ae6fb1af
Romeoisl <167251263+Romeoisl@users.noreply.github.com>
2026-09-24 11:56:14 +0100

    feat: consume remote call video frames in renderer

--- renderer/js/api.js
+++ renderer/js/api.js
@@ -3,7 +3,7 @@ const api = {
   handlers: { 
     connection: [], chats: [], messages: [], 
     presence: [], settings: [], schedules: [], calls: [],
-    call: [], 'call-state': [], 'call-error': [], 'call-audio': [], outbox: []
+    call: [], 'call-state': [], 'call-error': [], 'call-audio': [], 'call-video': [], outbox: []
   },
 
   async init() {
@@ -55,6 +55,7 @@ const api = {
     window.liquid.onCallState((state) => this._emit('call-state', state))
     window.liquid.onCallError((error) => this._emit('call-error', error))
     window.liquid.onCallAudio((audio) => this._emit('call-audio', audio))
+    window.liquid.onCallVideo((video) => this._emit('call-video', video))
     window.liquid.onCallRing((callData) => {
       console.log(`[API Interface] Intercepted active ring handshake protocol request token: ${callData.id}`)
       this._emit('call', callData)
